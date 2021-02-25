dc10baad8df22f7a59802303f164133fe1ab4f94
abhishek upadhyay <abhishekupadhyay483@gmail.com>
2021-02-25 10:32:11 +0530

    add

---
+++ app.js
@@ -0,0 +1,103 @@
+const express = require("express"),
+    app = express(),
+    mongoose = require("mongoose"),
+    bodyParser = require("body-parser"),
+    passport = require("passport"),
+    localStrategy = require("passport-local"),
+    flash = require('connect-flash'),
+    Admin = require("./models/admin"),
+    session = require('express-session'),
+    cookieParser = require('cookie-parser'),
+    methodOverride = require('method-override'),
+    port = process.env.PORT || 4000,
+    dotenv = require('dotenv'),
+    passportLocalMongoose = require("passport-local-mongoose");
+    var sslRedirect = require('heroku-ssl-redirect');
+dotenv.config();
+
+
+
+// app config-----
+app.use(cookieParser('secret'));
+app.use(require("express-session")({
+    secret: "This is a marketing panel",
+    resave: false,
+    saveUninitialized: false,
+    cookie:{
+      sameSite:"strict"
+    }
+
+}));
+app.use(express.static(__dirname + "/public/"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(methodOverride("_method"));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use(function (req, res, next) {
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+});
+app.use(sslRedirect());
+// Require Routes================================
+
+var userHomeRoutes=require("./routes/home"),
+    userProductRoutes=require("./routes/product"),
+    userCartRoutes =require("./routes/cart"),
+    userItemRoutes =require("./routes/item");
+
+// mongoose config
+// mongoose.connect("mongodb://localhost/stickman_real_art");
+// const mongoURI = "mongodb://localhost/stickman_apple_v1";
+// const mongoURI = "mongodb+srv://ankit:"+process.env.MLAB_PASS+"@cluster0-gyowo.mongodb.net/real_art?retryWrites=true&w=majority";
+const mongoURI = "mongodb+srv://ankit:"+process.env.mongo_pass+"@cluster0.f8aql.mongodb.net/mravans_admin_v1?retryWrites=true&w=majority";
+//Mongo connection
+mongoose.connect(mongoURI);
+
+//PASSPORT config
+
+// passport.use('admin', new localStrategy(Admin.authenticate()));
+passport.use('admin', new localStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+  }, Admin.authenticate()
+  ));
+  // passport.use('employee', new localStrategy(Employee.authenticate()));
+  passport.serializeUser(function (user, done) {
+    var key = {
+      id: user.id,
+      type: user.typeof
+    }
+    done(null, key);
+  })
+  passport.deserializeUser(function (key, done) {
+    if (key.type === 'admin') {
+      Admin.findOne({
+        _id: key.id
+      }, function (err, user) {
+        done(err, user);
+      })
+    }
+  
+  })
+
+// Use Routes======================
+app.use("/",userHomeRoutes);
+app.use("/products",userProductRoutes);
+app.use("/cart",userCartRoutes);
+app.use("/product",userItemRoutes);
+
+app.get("*",(req,res)=>{
+  res.redirect("/");
+  // res.render("error")
+})
+
+
+app.listen(port, () => {
+    console.log("Server Started on " + port);
+})
+
